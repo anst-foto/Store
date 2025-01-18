e198f4d14fbbc5a98115ad9d0e98a9d7b0a0bced
Starinin Andrey <starinin-andrey@ya.ru>
2025-01-18 12:19:07 +0300

    Добавление миграции

---
+++ Store.DB/StoreContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Store.DB;
+
+public class StoreContextFactory : IDesignTimeDbContextFactory<StoreContext>
+{
+    public StoreContext CreateDbContext(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+#if DEBUG
+        var connectionString = config.GetConnectionString("TestConnection");
+#elif RELEASE
+        var connectionString = config.GetConnectionString("PublicConnection");
+#endif
+
+        return new StoreContext(connectionString);
+    }
+}
